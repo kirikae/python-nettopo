8fc15cccb156adb40917945c9e881b8e83474c0d
Kirikae <22813189+kirikae@users.noreply.github.com>
2019-07-20 19:26:45 +1000

    Removes unused section of code

--- nstopo.py
+++ nstopo.py
@@ -91,11 +91,3 @@ def NetstatData():
             print(add_relationship)
 
 NetstatData()
-
-    for file in system.path(arg):
-        for info in file:
-            sline = line.strip().split(None, 8)
-            if "tcp" in sline[0]:
-                data.append((sline[0], sline[3], sline[4], sline[5], sline[6]))
-            elif "udp" in sline[0]:
-                data.append((sline[0], sline[3], sline[4], None, sline[6]))
