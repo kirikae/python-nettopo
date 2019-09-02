54ca8ad9810d0fd2baaa02caef8fb71f862f6d45
Kirikae <22813189+kirikae@users.noreply.github.com>
2019-09-02 18:15:40 +1000

    Update nstopo.py for Applicationand UDP support

--- nstopo.py
+++ nstopo.py
@@ -1,93 +1,107 @@
 #!/usr/bin/env python
 
+import socket
 import argparse
+from getpass import getpass
 from collections import namedtuple
 from neo4j import GraphDatabase
 
 
+neo4jusername = input("Neo4j username: ")
+neo4jpassword = getpass("Neo4j password: ")
 uri = "bolt://localhost:7687"
-driver = GraphDatabase.driver(uri, auth=('neo4j', 'test'))
-hosts = []
+driver = GraphDatabase.driver(uri, auth=(neo4jusername, neo4jpassword))
+
+Hosts = []
 Sources = {}
 
 parser = argparse.ArgumentParser(
-    description = "netstat output files to graph"
-)
-parser.add_argument('-f', nargs='*')
+    description = "Parse Netstat output files and import them into Neo4j Graph Database"
+    )
+parser.add_argument('--files', '-f', nargs='+', help='Pass the File or Files to analyse')
 
 args = parser.parse_args()
 
-def parsefilename(filename):
-    return filename.split('/')[-2]
-
 for arg in vars(args):
-    files = getattr(args, arg)
+    filenamess = getattr(args, arg)
 
-def NetstatData():
+def main():
     Connection = namedtuple('Connection', ['proto', 'recvq', 'sendq', 'localaddr', 'localport', 'remoteaddr', 'remoteport', 'state', 'pid', 'program'])
     all_conections = []
-    header_test = ['Active', 'Proto']
 
-    for arg in files:
-        lines = []
-        with open(arg, 'r') as f:
-            lines = f.read()
-        if not any(text in lines for text in header_text):
-            continue
-        else:
-            lines = lines.split('\n')[2:-1]
-        for line in lines:
-            if "tcp" in line.split()[0]:
-                (proto, recvq, sendq, local, remote, state, pidprog) = line.split()[7]
-            elif "udp" in line.split()[0]:
-                (proto, recvq, sendq, local, remote, state, pidprog) = line.split()[4], "", line.split()[5]
+    for File in filenames:
+        print("Reading in file: ",File)
+        with open(File, 'r') as f:
+            for line in f:
+                current_line = line.split()
+                if current_line[0] == "tcp":
+                    (proto, recvq, sendq, local, remote, state, pidprog) = current_line
+                elif current_line[0] == "udp":
+                    if current_line[5] == "ESTABLISHED":
+                        (proto, recvq, sendq, local, remote, state, pidprog) = current_line
+                    else:
+                        state = ""
+                        (proto, recvq, sendq, local, remote, pidprog) = current_line
+                else:
+                    continue
+                if pidprog == '-':
+                    (pid, program) = ("UNKNOWN", "UNKNOWN")
+                else:
+                    (pid, program) = pidprog.split('/')
 
-            pidprog = pidprog.split('/')
-            if pidprog[0] == '-':
-                (pid, program) = (pidprog, pidprog)
-            else:
-                (pid, program) = (pidprog[0], pidprog[1])
+                (localaddr, localport) = local.split(':')[:2]
+                (remoteaddr, remoteport) = remote.split(':')[:2]
 
-            (localaddr, localport) = local.split(':')[:2]
-            (remoteaddr, remoteport) = remote.split(':')[:2]
-
-            current_connection = Connection(proto, recvq, sendq, localaddr, localport, remoteaddr, remoteport, state, pid, program)
-            if "LISTEN" not in current_connection[7]:
-                all_connections.append(current_connection)
-
-    print(all_connections)
+                current_connection = Connection(proto, recvq, sendq, localaddr, localport, remoteaddr, remoteport, state, pid, program)
+        print("Finished with :", File)
 
     alladdrs = set()
     localconns = set()
     remoteconns = set()
+    allprograms = set()
+    Host = namedtuple('Host', ['hostname', 'ipaddr'])
+    all_hosts = []
+    all_programs = []
 
     for conn in all_connections:
         alladdrs.add(conn.localaddr)
         alladdrs.add(conn.remoteaddr)
         localconns.add(conn.localaddr)
         remotecons.add(conn.remoteaddr)
-
-    print(alladdrs)
+        allprograms.add(conn.program)
 
     for host in alladdrs:
-        add_host = "CREATE (A:COMPUTER {Name: \"" + host + "\"})"
+        try:
+            name = socket.gethostbyaddr(host)[0]
+        except:
+            name = "UNKNOWN"
+        add_host = "CREATE (A:COMPUTER {IP: \"" + host + "\", FQDN: \"" + name "\"})"
 
         with driver.session() as session:
             session.run(add_host)
             session.close()
-        print(add_host)
+
+    for program in allprograms:
+        add_program = "CREATE (A:PROGRAM {Name: \"" + program + "\"})"
+
+        with driver.session() as session:
+            session.run(add_program)
+            session.close()
+
 
     for conn in all_connections:
         if conn.localaddr not in Sources.keys():
             Sources[conn.localaddr] = []
         if conn.remoteaddr not in Sources[conn.localaddr]:
-            add_relationship = "MATCH (A:COMPUTER {Name: \"" + conn.localaddr + "\"}),(B:COMPUTER {Name: \"" + conn.remoteaddr + "\"}) CREATE (A)-[:CONNECTS_TO]->(B)"
+            add_app_relationship = "MATCH (A:COMPUTER {IP: \"" + conn.localaddr + "\"}),(B:PROGRAM {Name: \"" + conn.program + "\"}),(C:COMPUTER {IP: \"" + conn.remoteaddr + "\"}) CREATE (A)-[:RUNS]->(B)"
+            add_hosts_relationship = "MATCH (A:COMPUTER {IP: \"" + conn.localaddr + "\"}),(B:PROGRAM {Name: \"" + conn.program + "\"}),(C:COMPUTER {IP: \"" + conn.remoteaddr + "\"}) CREATE (B)-[:CONNECTS_TO {Local_Port: \"" + conn.localport + "\", Remote_Port: \"" + con.remoteport + "\", Protocol: \"" + conn.proto + "\", State: \"" + conn.state + "\"}]->(C)"
 
             with driver.session() as session:
-                session.run(add_relationship)
+                session.run(add_app_relationship)
+                session.run(add_host_relationship)
                 Sources[conn.localaddr].append(conn.remoteaddr)
                 session.close()
 
-            print(add_relationship)
 
-NetstatData()
+if __name__ = '__main__':
+    main()
