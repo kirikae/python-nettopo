b60dfe87e6d1a2d4cdbb895d54ecb59b39bd5095
Tristan Steele <tristan.steele@gmail.com>
2019-09-03 15:38:41 +1000

    refactor to buffer n4j calls

--- nstopo.py
+++ nstopo.py
@@ -6,32 +6,85 @@ from getpass import getpass
 from collections import namedtuple
 from neo4j import GraphDatabase
 
+##########################
+# Create namedtuples used
+# ------------------------
+Connection = namedtuple('Connection', ['proto', 'recvq', 'sendq', 'localaddr', 'localport', 'remoteaddr', 'remoteport', 'state', 'pid', 'program'])
+Host = namedtuple('Host', ['hostname', 'ipaddr'])
 
-neo4jusername = input("Neo4j username: ")
-neo4jpassword = getpass("Neo4j password: ")
-uri = "bolt://localhost:7687"
-driver = GraphDatabase.driver(uri, auth=(neo4jusername, neo4jpassword))
+##########################
+# Constants
+# ------------------------
+URI = "bolt://localhost:7687"
 
-Hosts = []
-Sources = {}
 
+##########################
+# Parse Arguments
+# ------------------------
 parser = argparse.ArgumentParser(
     description = "Parse Netstat output files and import them into Neo4j Graph Database"
     )
 parser.add_argument('--files', '-f', nargs='+', help='Pass the File or Files to analyse')
-
 args = parser.parse_args()
-
 for arg in vars(args):
     filenames = getattr(args, arg)
 
-def main():
-    Connection = namedtuple('Connection', ['proto', 'recvq', 'sendq', 'localaddr', 'localport', 'remoteaddr', 'remoteport', 'state', 'pid', 'program'])
-    all_connections = []
+# Create driver for neo4j:
+# TODO: Allow these to be parsed in from args or ENV
+neo4jusername = input("Neo4j username: ")
+neo4jpassword = getpass("Neo4j password: ")
+driver = GraphDatabase.driver(URI, auth=(neo4jusername, neo4jpassword))
 
-    for File in filenames:
-        print("Reading in file: ",File)
-        with open(File, 'r') as f:
+class N4JQueue(set):
+    def __init__(self, driver: GraphDatabase.driver, buffer_size: int = 10000):
+        self._driver = driver
+        self._buffer_size = buffer_size
+
+    def submit(self):
+        with self._driver.session() as session:
+            for item in self:
+                try:
+                    session.run(item)
+                    self.remove(item)
+                except Exception as e:
+                    print("Issue with submitting or removing item: {}".format(e))
+
+    def add(self, element) -> None:
+        super().add(element= element)
+
+        if len(self) >= self._buffer_size:
+            self.submit()
+
+class CachedDNSLookup:
+    """
+    This class provides a simple dictionary backed cache for hostname lookups.
+    """
+    def __init__(self):
+        self._cache= {}
+
+    def get_host_by_address(self, addr):
+        if addr not in self._cache.keys():
+            self._cache[addr] = socket.gethostbyaddr(addr)[0]
+        return self._cache[addr]
+
+
+def main():
+    """
+    The main entry point!
+    :return: None
+    """
+
+    all_connections = []
+    sources = {}
+
+    dns_lookup = CachedDNSLookup()
+
+    n4j_queue = N4JQueue(driver, buffer_size=10000)
+
+    # First, we read this into an array, parsing as we go.
+    for file in filenames:
+        print("Reading in file: ",file)
+        with open(file, 'r') as f:
             for line in f:
                 current_line = line.split()
                 print(current_line)
@@ -56,57 +109,65 @@ def main():
                 current_connection = Connection(proto, recvq, sendq, localaddr, localport, remoteaddr, remoteport, state, pid, program)
                 all_connections.append(current_connection)
 
-        print("Finished with :", File)
+        print("Finished with :", file)
 
-    alladdrs = set()
-    localconns = set()
-    remoteconns = set()
-    allprograms = set()
-    Host = namedtuple('Host', ['hostname', 'ipaddr'])
-    all_hosts = []
-    all_programs = []
+    # Create working Sets
+    all_addresses = set()
+    local_connection = set()
+    remote_connections = set()
+    all_programs = set()
 
     for conn in all_connections:
-        alladdrs.add(conn.localaddr)
-        alladdrs.add(conn.remoteaddr)
-        localconns.add(conn.localaddr)
-        remoteconns.add(conn.remoteaddr)
-        allprograms.add(conn.program)
+        all_addresses.add(conn.localaddr)
+        all_addresses.add(conn.remoteaddr)
+        local_connection.add(conn.localaddr)
+        remote_connections.add(conn.remoteaddr)
+        all_programs.add(conn.program)
 
-    for host in alladdrs:
+    print("Connections loaded.")
+
+    for host in all_addresses:
         try:
-            name = socket.gethostbyaddr(host)[0]
+            name = dns_lookup.get_host_by_address(host)
         except:
             name = "UNKNOWN"
-        add_host = "CREATE (A:COMPUTER {IP: \"" + host + "\", FQDN: \"" + name + "\"})"
 
-        with driver.session() as session:
-            session.run(add_host)
-            session.close()
+        add_host_str = 'CREATE (A:COMPUTER {IP: "{}", FQDN: "{}"})'.format(host, name)
+        n4j_queue.add(add_host_str)
+    print("Hosts loaded.")
 
-    for program in allprograms:
-        add_program = "CREATE (A:PROGRAM {Name: \"" + program + "\"})"
-
-        with driver.session() as session:
-            session.run(add_program)
-            session.close()
+    for program in all_programs:
+        add_program = 'CREATE (A:PROGRAM {Name: "{}"})'.format(program)
+        n4j_queue.add(add_program)
+    print("Programs loaded.")
 
     for conn in all_connections:
-        if conn.localaddr not in Sources.keys():
-            Sources[conn.localaddr] = []
-        if conn.remoteaddr not in Sources[conn.localaddr]:
-            add_app_relationship = "MATCH (A:COMPUTER {IP: \"" + conn.localaddr + "\"}),(B:PROGRAM {Name: \"" + conn.program + "\"}),(C:COMPUTER {IP: \"" + conn.remoteaddr + "\"}) CREATE (A)-[:RUNS]->(B)"
-            add_host_relationship = "MATCH (A:COMPUTER {IP: \"" + conn.localaddr + "\"}),(B:PROGRAM {Name: \"" + conn.program + "\"}),(C:COMPUTER {IP: \"" + conn.remoteaddr + "\"}) CREATE (B)-[:CONNECTS_TO {Local_Port: \"" + conn.localport + "\", Remote_Port: \"" + conn.remoteport + "\", Protocol: \"" + conn.proto + "\", State: \"" + conn.state + "\"}]->(C)"
+        if conn.localaddr not in sources.keys():
+            sources[conn.localaddr] = []
+        if conn.remoteaddr not in sources[conn.localaddr]:
+            add_app_relationship =  f'MATCH (A:COMPUTER {{IP: "{conn.localaddr}"}}),'\
+                                    f'(B:PROGRAM {{Name: "{conn.program}"}}),'\
+                                    f'(C:COMPUTER {{IP: "{conn.remoteaddr}"}}) CREATE (A)-[:RUNS]->(B)'
 
-            with driver.session() as session:
-                session.run(add_app_relationship)
-                session.run(add_host_relationship)
-                Sources[conn.localaddr].append(conn.remoteaddr)
-                session.close()
+            add_host_relationship = f'MATCH (A:COMPUTER {{IP: "{conn.localaddr}"}}),'\
+                                    f'(B:PROGRAM {{Name: "{conn.program}"}}),'\
+                                    f'(C:COMPUTER {{IP: "{conn.remoteaddr}"}}) '\
+                                    f'CREATE (B)-[:CONNECTS_TO {{Local_Port: "{conn.localport}", '\
+                                    f'Remote_Port: "{conn.remoteport}", '\
+                                    f'Protocol: "{conn.proto}", '\
+                                    f'State: "{conn.state}"}}]->(C)'
 
-        print(add_app_relationship)
-        print(add_host_relationship)
+            n4j_queue.add(add_app_relationship)
+            n4j_queue.add(add_host_relationship)
 
+            sources[conn.localaddr].append(conn.remoteaddr)
+
+            print(add_app_relationship)
+            print(add_host_relationship)
+    print("Connections loaded.")
+
+    # Write remaining contents of the buffer.
+    n4j_queue.submit()
 
 if __name__ == '__main__':
     main()
